54b6df1b39177d6d8253cfad75e1a12c9e3f06e9
Alexandre Andrade <ama@esss.com.br>
2015-03-30 22:10:23 -0300

    Refactoring app to rely on configuration.

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,14 @@ RUN apt-get update
 RUN apt-get install -y nginx redis-server supervisor python3-pip git
 
 # Update working directories
-ADD ./config /config
+ADD ./config /gir/config
 
 # Configure python-rq
 ENV LC_ALL=C.UTF-8
 ENV LANG=C.UTF-8
 
 # Install application requirements (python3)
-RUN pip3 install -r /config/requirements.txt
+RUN pip3 install -r /gir/config/requirements.txt
 
 # Configure redis-server
 # . No daemon since we're using supervisord
@@ -26,13 +26,14 @@ RUN sed -i 's/^\(daemonize\s*\)yes\s*$/\1no/g' /etc/redis/redis.conf
 RUN echo "\ndaemon off;" >> /etc/nginx/nginx.conf
 # . Replace default site by nginx.conf
 RUN rm /etc/nginx/sites-enabled/default
-RUN ln -s /config/nginx.conf /etc/nginx/sites-enabled/
+RUN ln -s /gir/config/nginx.conf /etc/nginx/sites-enabled/
 
 # Configure supervisor
-RUN ln -s /config/supervisor.conf /etc/supervisor/conf.d/
+RUN ln -s /gir/config/supervisor.conf /etc/supervisor/conf.d/
 
-# Add app last to easy changes in the code
-ADD ./app /app
+# Add these last to avoid unnecessary changes in the container
+ADD ./app /gir/app
+ADD ./static /gir/static
 
 EXPOSE 80
 CMD ["supervisord", "-n"]
--- app/app.py
+++ app/app.py
@@ -11,84 +11,95 @@ RQDashboard(app)
 
 slack = Slack()
 
+CONFIG ={
+    'stash' : dict(
+        message = 'Commit on `repository.slug`',
+        icon_url = 'https://developer.atlassian.com/imgs/stash.png',
+        username = 'Stash',
+    ),
+    'jira' : dict(
+        message = '<`issue.self`|`issue.key`>: `issue.fields.summary` [@`user.name`]',
+        icon_url = 'https://developer.atlassian.com/imgs/jira.png',
+        username = 'Jira',
+    ),
+    'message' : dict(
+        message = '`message`',
+        icon_url = 'http://static.tumblr.com/2qdysyt/AaTm73sce/gir_sitting.png',
+        username = '`user`@`host`',
+    ),
+    'jenkins' : dict(
+        message = 'Job `emoji` <https://eden.esss.com.br/jenkins/`url`|`name`> <`build.full_url`|#`build.number`>.',
+        icon_url = 'https://slack.global.ssl.fastly.net/20653/img/services/jenkins-ci_48.png',
+        username = 'Jenkins',
+        remapping = {
+            'emoji' : {
+                '__key'               : '`build.phase`, `build.status`',
+                '__default'           : '(`build.phase`, `build.status`) :question:',
+                'STARTED, '           : ':arrow_forward:',
+                'FINALIZED, FAILURE'  : ':no_entry:',
+                'FINALIZED, SUCCESS'  : ':white_check_mark:',
+                'FINALIZED, ABORTED'  : ':warning:',
+                'FINALIZED, UNSTABLE' : ':warning:',
+            }
+        },
+        early_exit = {
+            (
+                '"`build.phase`" == "COMPLETED"',
+                'Ignore COMPLETED events in favor of FINALIZED (after all post-build).',
+                'OK',
+            ),
+        },
+    ),
+}
+
+class Handler(object):
+
+    def __init__(self, config):
+        self.__config = config
+
+    def __call__(self):
+        return self.HandleIt(
+            self.__config['message'],
+            self.__config['icon_url'],
+            self.__config['username'],
+            self.__config.get('remapping', {}),
+            self.__config.get('early_exit', {})
+        )
+
+    @classmethod
+    def HandleIt(cls, message, icon_url, username, remapping={}, early_exit=[]):
+        data = json.loads(request.data)
+
+        # Create new values on data based on remapping dictionary.
+        # Check Remapping function for more details.
+        # Example:
+        #   We use a different emoji depending on the combination of a build (jenkins) phase and status.
+        for i_key, i_mapping in remapping.iteritems():
+            data[i_key] = Remapping(i_mapping, data)
+
+        # Exits without sending the message to Slack under some conditions.
+        for i_expression, i_description, i_result in early_exit:
+            i_expression = JsonSub(i_expression, data)
+            if eval(i_expression):
+                return i_result
+
+        # Sends the message (using queue)
+        message = JsonSub(message, data)
+        username = JsonSub(username, data)
+        slack.Message(message, icon_url, username)
+
+        return 'OK'
+
+
+
 @app.route("/")
 def index():
     return "Hello, from GIR!"
 
 
-@app.route("/message", methods=['POST'])
-def message():
-    message = '`message`'
-    icon_url = 'http://static.tumblr.com/2qdysyt/AaTm73sce/gir_sitting.png'
-    username = '`user`@`host`'
-    return HandleIt(message, icon_url, username)
-
-
-@app.route("/jenkins", methods=['POST'])
-def jenkins():
-    message = 'Job `emoji` <https://eden.esss.com.br/jenkins/`url`|`name`> <`build.full_url`|#`build.number`>.'
-    icon_url = 'https://slack.global.ssl.fastly.net/20653/img/services/jenkins-ci_48.png'
-    username = 'Jenkins'
-    REMAPPING = {
-        'emoji' : {
-            '__key' : '`build.phase`, `build.status`',
-            '__default' : '(`build.phase`, `build.status`) :question:',
-            'STARTED, ' : ':arrow_forward:',
-            'FINALIZED, FAILURE' : ':no_entry:',
-            'FINALIZED, SUCCESS' : ':white_check_mark:',
-            'FINALIZED, ABORTED' : ':warning:',
-            'FINALIZED, UNSTABLE' : ':warning:',
-        }
-    }
-    EARLY_EXIT = {
-        (
-            '"`build.phase`" == "COMPLETED"',
-            'Ignore COMPLETED events in favor of FINALIZED (after all post-build).',
-            'OK',
-        ),
-    }
-    return HandleIt(message, icon_url, username, REMAPPING, EARLY_EXIT)
-
-
-
-@app.route("/stash", methods=['POST'])
-def stash():
-    message = 'Commit on `repository.slug`'
-    icon_url = 'https://developer.atlassian.com/imgs/stash.png'
-    username = 'Stash'
-    return HandleIt(message, icon_url, username)
-
-
-@app.route("/jira", methods=['POST'])
-def jira():
-    message = '<`issue.self`|`issue.key`>: `issue.fields.summary` [@`user.name`]'
-    icon_url = 'https://developer.atlassian.com/imgs/jira.png'
-    username = 'Jira'
-    return HandleIt(message, icon_url, username)
-
-
-def HandleIt(message, icon_url, username, remapping={}, early_exit=[]):
-    data = json.loads(request.data)
-
-    # Create new values on data based on remapping dictionary.
-    # Check Remapping function for more details.
-    # Example:
-    #   We use a different emoji depending on the combination of a build (jenkins) phase and status.
-    for i_key, i_mapping in remapping.iteritems():
-        data[i_key] = Remapping(i_mapping, data)
-
-    # Exits without sending the message to Slack under some conditions.
-    for i_expression, i_description, i_result in early_exit:
-        i_expression = JsonSub(i_expression, data)
-        if eval(i_expression):
-            return i_result
-
-    # Sends the message (using queue)
-    message = JsonSub(message, data)
-    username = JsonSub(username, data)
-    slack.Message(message, icon_url, username)
-
-    return 'OK'
+for i_route, i_config in CONFIG.iteritems():
+    handler = Handler(i_config)
+    app.add_url_rule('/' + i_route, i_route, handler, methods=['POST'])
 
 
 if __name__ == "__main__":
