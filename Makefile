879f3bf0aa123a1dccbdd03636c9a6be1adc7131
Alexandre Andrade <ama@esss.com.br>
2015-04-22 21:34:45 -0300

    Use flask configuration instead of os.environ everywhere. (#2)

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-NAME = quay.io/kaniabi/gir
+CONTAINER_NAME = quay.io/kaniabi/gir
+NAME = gir
 VERSION = v0.1
 
 .PHONY: build run bash rm
@@ -6,13 +7,13 @@ VERSION = v0.1
 all: build
 
 build:
-	sudo docker build --rm -t $(NAME):$(VERSION) .
+	sudo docker build --rm -t $(CONTAINER_NAME):$(VERSION) .
 
 run:
-	sudo docker run -p 80:80 -e GIR_SLACK_ROOM=#bos-ama -e GIR_SLACK_TOKEN=xoxb-3724390083-OaNvuxp0CbYdo0fiy5kPBcWE -e GIR_FLASK_PORT=80 -e GIR_SLACK_USER=gir -e GIR_SLACK_HOST=docker -e GIR_STATIC_URL=http://188.226.245.90/static/ -e GIR_DEBUG=true -e DEBUG=true --rm=true --name gir $(NAME):$(VERSION)
+	sudo docker run -p 80:80 -e GIR_SLACK_ROOM=#bos-ama -e GIR_SLACK_TOKEN=xoxb-3724390083-OaNvuxp0CbYdo0fiy5kPBcWE -e GIR_FLASK_PORT=80 -e GIR_SLACK_USER=gir -e GIR_SLACK_HOST=docker -e GIR_STATIC_URL=http://188.226.245.90/static/ -e GIR_DEBUG=true -e DEBUG=true --rm=true --name $(NAME) $(CONTAINER_NAME):$(VERSION)
 
 bash:
 	sudo docker exec -t -i $(NAME) /bin/bash
 
 rm:
-	sudo docker rm gir
+	sudo docker rm $(NAME)
--- gir/worker.py
+++ gir/worker.py
@@ -1,49 +1,62 @@
 from __future__ import unicode_literals
 
 
-
-def StaticResource(filename):
+def GetConfig(name):
     import os
-    STATIC_URL = os.environ.get('GIR_STATIC_URL', 'http://188.226.245.90/static/')
-    return STATIC_URL + filename
+    import default_config
 
-
-def SendMessage(message, icon_url=None, username=None, room=None):
-    from slackclient import SlackClient
-    import getpass
-    import os
-    import socket
-
-    TOKEN = os.environ.get('SLACK_TOKEN')
-    assert TOKEN is not None, 'Please configure SLACK_TOKEN environment variable.'
-    USER = os.environ.get('GIR_USER', getpass.getuser())
-    HOST = os.environ.get('GIR_HOST', socket.gethostname())
-    icon_url = icon_url or StaticResource('gir_stare.png')
-    username = username or '%s@%s' % (USER, HOST)
-    room = room or os.environ.get('SLACK_ROOM', '#general')
-
-    slack = SlackClient(TOKEN)
-    slack.api_call(
-        'chat.postMessage',
-        token=TOKEN,
-        channel=room,
-        text=message,
-        icon_url=icon_url,
-        username=username,
-    )
+    result = os.environ.get('GIR_' + name)
+    if result is not None:
+        return result
+    return getattr(default_config, name)
 
 
 class Slack(object):
 
-    def __init__(self):
+    SLACK_TOKEN = GetConfig('SLACK_TOKEN')
+    SLACK_ROOM = GetConfig('SLACK_ROOM')
+    SLACK_USER = GetConfig('SLACK_USER')
+    SLACK_HOST = GetConfig('SLACK_HOST')
+    REDIS_SERVER = GetConfig('REDIS_SERVER')
+    REDIS_PORT = GetConfig('REDIS_PORT')
+    STATIC_URL = GetConfig('STATIC_URL')
+
+    def __init__(self, server=REDIS_SERVER, port=REDIS_PORT):
         from redis import Redis
         from rq import Queue
-        import os
-        redis_server = os.environ.get("REDIS_SERVER", 'localhost')
-        redis_port = int(os.environ.get("REDIS_PORT", 6379))
-        redis_connection = Redis(redis_server, redis_port)
-        self.__queue = Queue(connection=redis_connection)
+        connection = Redis(server, port)
+        self.__queue = Queue(connection=connection)
 
-    def Message(self, message, icon_url=None, username=None, room=None):
-        self.__queue.enqueue(SendMessage, message, icon_url, username, room)
 
+    def Message(self, message, icon_url=None, username=None, room=SLACK_ROOM):
+        import sys
+        if sys.platform == 'win32':
+            execute = lambda f, m, i, u, r: f(m,i,u,r)
+        else:
+            execute = self.__queue.enqueue
+        execute(Slack.SendMessage, message, icon_url, username, room)
+
+
+    @classmethod
+    def StaticResource(cls, filename):
+        return cls.STATIC_URL + filename
+
+
+    @classmethod
+    def SendMessage(cls, message, icon_url=None, username=None, room=SLACK_ROOM):
+        from slackclient import SlackClient
+
+        assert cls.SLACK_TOKEN is not None, 'Please configure SLACK_TOKEN flask configuration.'
+
+        icon_url = icon_url or cls.StaticResource('gir_stare.png')
+        username = username or '%s@%s' % (cls.SLACK_USER, cls.SLACK_HOST)
+
+        slack = SlackClient(cls.SLACK_TOKEN)
+        slack.api_call(
+            'chat.postMessage',
+            token=cls.SLACK_TOKEN,
+            channel=room,
+            text=message,
+            icon_url=icon_url,
+            username=username,
+        )
